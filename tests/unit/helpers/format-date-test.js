ffbff0ba0587460c30541fc460e1d5c0aa7bd91c
khanhnhat <nguyen.k21@gmail.com>
2016-04-06 23:27:34 -0400

    Automatic generating tests for format-date helper

---
+++ tests/unit/helpers/format-date-test.js
@@ -0,0 +1,10 @@
+import { formatDate } from 'taste-or-waste-template/helpers/format-date';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format date');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatDate([42]);
+  assert.ok(result);
+});
