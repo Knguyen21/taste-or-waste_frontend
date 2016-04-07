4447d1154c2a15aac0c06dab9395ae7187f27e8b
khanhnhat <nguyen.k21@gmail.com>
2016-04-06 23:25:28 -0400

    Fix the date error

--- app/components/food-form/component.js
+++ app/components/food-form/component.js
@@ -2,20 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   form: {},
+  foodProperties: Ember.computed('form', function(){
+    return {
+      storage: this.get('form.storage'),
+      disposal: this.get('form.disposal'),
+      name: this.get('form.name'),
+      description: this.get('form.description'),
+      purchasedDate: new Date(this.get('form.purchasedDate')),
+      expirationDate: new Date(this.get('form.expirationDate')),
+      remindDate: new Date(this.get('form.remindDate')),
+    };
+  }),
   actions: {
     createFood: function(){
       console.log('Component Action : createFood');
-      this.sendAction('routeCreateFood', this.get('form'));
+      debugger;
+      this.sendAction('routeCreateFood', this.get('foodProperties'));
       this.set('form', {});
     },
     selectValue () {
-    this.set('form.storage', Ember.$('select').val());
-    console.log(this);
-  },
-  dateValue () {
-  this.set('form.storage', Ember.$('select').val());
-  console.log(this);
-},
+      this.set('form.storage', Ember.$('select').val());
+      console.log(this);
+    },
   }
-
 });
