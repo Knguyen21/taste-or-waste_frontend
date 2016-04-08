1845cbfda7173b04706efd30c908fcd7919e57b4
khanhnhat <nguyen.k21@gmail.com>
2016-04-08 02:57:43 -0400

    remove food form link on storage page
    I also made some updates in the model, but tried to change it back to the original version.

--- app/food/model.js
+++ app/food/model.js
@@ -8,15 +8,7 @@ export default DS.Model.extend({
   category: DS.attr('string'),
   name: DS.attr('string'),
   description: DS.attr('string'),
-  purchasedDate: DS.attr('date', {defaultValue: new Date()}),
-  expirationDate: DS.attr('date', {defaultValue: new Date()}),
-  remindDate: DS.attr('date', {defaultValue: new Date()}),
-  counter: 0,
-  count: Ember.computed(function(){
-    let total= this.get('counter');
-    total++;
-    let newtotal= this.set('counter', total);
-    return newtotal;
-
-  })
+  purchasedDate: DS.attr('date'),
+  expirationDate: DS.attr('date'),
+  remindDate: DS.attr('date')
 });
